bb6ee156d44b514dab51d02d10b121af3843fdd7
rotegott <pluto@rgott.com>
2023-06-26 21:54:57 +0300

    refactoring

--- example.js
+++ example.js
@@ -8,7 +8,7 @@ async function run() {
     { input: "Hello my friend!", output: 0 },
     { input: "Hey, hi", output: 0 },
     { input: "Bye bye!", output: 1 },
-    { input: "Bye", output: 1 },
+    { input: "Bye, no hello", output: 1 }, // edit
     { input: "See you later!", output: 1 },
     { input: "Goodbye dude!", output: 1 },
     { input: "Good bye", output: 1 },
